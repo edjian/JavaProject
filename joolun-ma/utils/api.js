1780ce67e0d53176edc61f1087ecd7d6d183705c
JL <535382760@qq.com>
2019-12-30 18:44:28 +0800

    更新 'joolun-ma/utils/api.js'
    修复收货地址不能删除的问题

--- joolun-ma/utils/api.js
+++ joolun-ma/utils/api.js
@@ -160,6 +160,9 @@ module.exports = {
   userAddressSave: (data) => {
     return request('/mall/api/ma/useraddress', 'post', data, true)
   },
+  userAddressDel: (id) => {
+    return request('/mall/api/ma/useraddress/' + id, 'delete', null, false)
+  },
   userCollectAdd: (data) => {
     return request('/mall/api/ma/usercollect', 'post', data, true)
   },
